8fbd9c4ffa4b9f63b5ef924715980ff36ea3c552
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2025-11-10 22:17:56 +0800

    added bitton

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,28 @@
-import { Calculator } from "lucide-react";
-import React from "react";
+import { Calculator, MoveRight } from "lucide-react";
+import React, { useContext } from "react";
+import { DarkModeContext } from "../../contexts/DarkModeContext";
 
 const Home = () => {
+  const { darkMode } = useContext(DarkModeContext);
   return (
-    <main className="w-7xl">
-      <div className="h-[300px] w-[300px] border-2"></div>
-      <div className="h-[300px] w-[300px] border-2"></div>
-      <div className="h-[300px] w-[300px] border-2"></div>
-      <div className="h-[300px] w-[300px] border-2"></div>
+    <main className="w-7xl mt-2">
+      <div className=" h-auto max-w-2xs cursor-pointer hover:scale-102 shadow-lg shadow-black dark:shadow-white rounded-3xl flex flex-col justify-between align-middle items-center">
+        <div className="flex flex-col align-middle items-center p-2 gap-1">
+          <Calculator size={100} color={darkMode ? "white" : `#191919`} />
+
+          <p className="text-sm">
+            Calculates Camco standard specifications & tolerances.
+          </p>
+        </div>
+        <div className="cursor-pointer w-full bottom-0 flex flex-col items-center bg-[#ffcc00] rounded-b-3xl p-1">
+          <button className="flex gap-2 dark:text-black">
+            Tolerance Checker{" "}
+            <span>
+              <MoveRight />
+            </span>
+          </button>
+        </div>
+      </div>
     </main>
   );
 };
