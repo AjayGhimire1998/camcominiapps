906a6a0d3379b98410ad425c496a51956655fc68
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2025-11-11 18:50:32 +0800

    new commit

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -6,7 +6,10 @@ const Header = () => {
     <header className=" top-0 sticky w-full  bg-white dark:bg-[#191919] dark:text-white z-50 flex justify-center ">
       <div className="w-[1350px] flex justify-between items-center align-middle p-4 max-sm:p-4 m-6  max-sm:m-4 shadow-sm dark:shadow-amber-50 dark:bg-[#121212] bg-[#ffcc00] shadow-black rounded-4xl">
         <div>
-          <Link to="/" className="font-extrabold text-[35px] ">
+          <Link
+            to="/"
+            className="font-extrabold text-[35px] max-sm:text-[25px] "
+          >
             MiniAPPS
           </Link>
         </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,23 +6,24 @@ import { Link } from "react-router-dom";
 const Home = () => {
   const { darkMode } = useContext(DarkModeContext);
   return (
-    <main className="md:w-7xl mt-2 max-sm:flex max-sm:items-center max-smjustify-center px-8 max-sm:p-4 flex  gap-20">
+    <main className="md:w-7xl mt-2 max-sm:flex max-sm:items-center max-smjustify-center px-8 max-sm:p-4 flex  gap-5">
       <Link to="/tolerance-checker">
-        <div className=" h-[200px]  max-w-2xs cursor-pointer border hover:scale-102 shadow-lg shadow-black dark:shadow-white rounded-3xl flex flex-col justify-between align-middle items-center ">
+        <div className=" h-auto max-w-2xs cursor-pointer border hover:scale-102 shadow-lg shadow-black dark:shadow-white rounded-3xl flex flex-col justify-between align-middle items-center ">
           <div className="flex flex-col align-middle items-center p-2 gap-1">
             <Shapes
               size={100}
               strokeWidth="0.5px"
               color={darkMode ? "white" : `#191919`}
+              className="max-sm:w-[65px]"
             />
 
-            <p className="text-sm">
-              Calculates standard specifications & tolerances.
+            <p className="text-sm max-sm:text-xs ">
+              Calculates specifications & tolerances.
             </p>
           </div>
           <div className=" w-full bottom-0 flex flex-col  bg-[#ffcc00] rounded-b-3xl p-1 ">
-            <button className="flex items-center align-middle justify-center gap-2 dark:text-black cursor-pointer text-lg font-extrabold">
-              Tolerance Checker{" "}
+            <button className="flex items-center align-middle justify-center gap-2 dark:text-black cursor-pointer text-lg font-semibold max-sm:text-sm">
+              Tolerance Checker
               <span>
                 <MoveRight strokeWidth="3px" />
               </span>
@@ -32,24 +33,27 @@ const Home = () => {
       </Link>
 
       <Link to="/">
-        <div className=" h-[200px] max-w-2xs cursor-pointer border hover:scale-102 shadow-lg shadow-black dark:shadow-white rounded-3xl flex flex-col justify-between align-middle items-center opacity-50 ">
+        <div className=" absolute bg-[#ffcc00] rounded-4xl px-2 flex">
+          <span className="text-[10px] font-semibold dark:text-black">
+            Coming soon
+          </span>
+        </div>
+        <div className=" h-auto max-w-2xs  border hover:scale-102 shadow-lg shadow-black dark:shadow-white rounded-3xl flex flex-col justify-between align-middle items-center opacity-50 cursor-not-allowed">
           <div className="flex flex-col align-middle items-center p-2 gap-1">
-            <div className=" absolute bg-[#ffcc00] rounded-4xl px-2 ">
-              <span className="text-[10px] font-semibold">Coming soon</span>
-            </div>
             <Radical
               size={100}
               strokeWidth="0.5px"
               color={darkMode ? "white" : `#191919`}
+              className="max-sm:w-[65px]"
             />
 
-            <p className="text-sm">
+            <p className="text-sm max-sm:text-xs ">
               Checks Bearings and their Radial Clearances.
             </p>
           </div>
           <div className=" w-full bottom-0 flex flex-col  bg-[#ffcc00] rounded-b-3xl p-1 ">
-            <button className="flex items-center align-middle justify-center gap-2 dark:text-black cursor-pointer text-lg font-extrabold">
-              Tolerance Checker{" "}
+            <button className="flex items-center align-middle justify-center gap-2 dark:text-black cursor-pointer text-lg font-semibold max-sm:text-xs">
+              Tolerance Checker
               <span>
                 <MoveRight strokeWidth="3px" />
               </span>
