403fc647d450da59f47b269f83e5b555d73720ec
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2025-11-14 20:04:17 +0800

    cahnges

--- src/pages/ToleranceChecker/ToleranceChecker.jsx
+++ src/pages/ToleranceChecker/ToleranceChecker.jsx
@@ -5,10 +5,6 @@ import { DarkModeContext } from "../../contexts/DarkModeContext";
 import Dropdown from "./components/Dropdown";
 
 const ToleranceChecker = () => {
-  //   const { darkMode } = useContext(DarkModeContext);
-  const [dropdownRequested, setDropdownRequested] = useState("");
-  const [dropdownVisible, setDropdownVisible] = useState(false);
-
   return (
     <main className="w-full max-w-[1350px] px-6 mt-2 ">
       <div className="  flex justify-start align-middle items-start mb-2">
@@ -21,58 +17,9 @@ const ToleranceChecker = () => {
         </Link>
       </div>
       <div className="flex max-[690px]:flex-col gap-5 max-sm:gap-2 justify-end mt-6 font-semibold ">
-        <div className="flex gap-1 max-sm:text-sm">
-          <span>Material Type </span>
-          <div className="">
-            <button
-              onClick={() => {
-                setDropdownRequested("material");
-                setDropdownVisible(!dropdownVisible);
-              }}
-              className="border rounded-md flex align-middle items-center px-2 py-0.5 max-sm:px-1.5 bg-[#ffcc00] dark:bg-[#fefefe] dark:text-black max-sm:text-xs"
-            >
-              Housing Bores <ChevronDown size={20} />
-            </button>
-            {dropdownRequested === "material" && dropdownVisible ? (
-              <Dropdown type={"material"} />
-            ) : null}
-          </div>
-        </div>
-
-        <div className="flex gap-1 max-sm:text-sm">
-          <span>Specification </span>
-          <div className="">
-            <button
-              onClick={() => {
-                setDropdownRequested("spec");
-                setDropdownVisible(!dropdownVisible);
-              }}
-              className="border rounded-md flex align-middle items-center px-2 py-0.5  max-sm:px-1.5 bg-[#ffcc00] dark:bg-[#fefefe] dark:text-black max-sm:text-xs"
-            >
-              H8 <ChevronDown size={20} />
-            </button>
-            {dropdownRequested === "spec" && dropdownVisible ? (
-              <Dropdown type={"spec"} />
-            ) : null}
-          </div>
-        </div>
-        <div className="flex gap-1 max-sm:text-sm">
-          <span>IT Grade</span>
-          <div className="">
-            <button
-              onClick={() => {
-                setDropdownRequested("it");
-                setDropdownVisible(!dropdownVisible);
-              }}
-              className="border rounded-md flex align-middle items-center px-2 py-0.5 max-sm:px-1.5 bg-[#ffcc00] dark:bg-[#fefefe] dark:text-black max-sm:text-xs"
-            >
-              IT6 <ChevronDown size={20} />
-            </button>
-            {dropdownRequested === "it" && dropdownVisible ? (
-              <Dropdown type={"it"} />
-            ) : null}
-          </div>
-        </div>
+        <Dropdown type={"material"} label={"Material Type"} />
+        <Dropdown type={"spec"} label={"Specification"} />
+        <Dropdown type={"it"} label={"IT"} />
       </div>
     </main>
   );
--- src/pages/ToleranceChecker/components/Dropdown.jsx
+++ src/pages/ToleranceChecker/components/Dropdown.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { BookDownIcon, ChevronDown, MoveLeft } from "lucide-react";
 
-const Dropdown = ({ type }) => {
+const Dropdown = ({ label, type }) => {
   const dropdownValues = {
     material: ["Housing Bores", "Shell Bores", "Shaft"],
     spec: ["H5", "H6", "H7", "H8", "H9", "h7", "h8", "h9"],
@@ -9,23 +10,43 @@ const Dropdown = ({ type }) => {
 
   const [typeRequested, setTypeRequested] = useState(type);
 
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
   useEffect(() => {
     setTypeRequested(type);
   }, []);
   return (
-    <div className="w-auto border absolute flex flex-col justify-end mt-1 rounded-md bg-[#ffcc00] dark:bg-[#fefefe] dark:text-[#000000] ">
-      {dropdownValues[typeRequested].map((val, i) => {
-        return (
-          <div className="border-b flex items-center align-middle justify-center ">
-            <div
-              id={i}
-              className="w-full m-0.5 cursor-pointer rounded-md hover:bg-[#f4c301] dark:hover:bg-gray-200"
-            >
-              <span>{val}</span>
-            </div>
+    <div className="flex gap-1 max-sm:text-sm  align-middle items-center">
+      <span>{label}</span>
+      <div className="">
+        <button
+          onClick={() => {
+            setDropdownVisible(!dropdownVisible);
+          }}
+          className="border rounded-md flex align-middle items-center px-2 py-0.5 max-sm:px-1.5 bg-[#ffcc00] dark:bg-[#fefefe] dark:text-black max-sm:text-xs"
+        >
+          {dropdownValues[type][0]} <ChevronDown size={20} />
+        </button>
+        {dropdownVisible ? (
+          <div className="w-auto border  flex flex-col justify-end mt-1 rounded-md bg-[#ffcc00] dark:bg-[#fefefe] dark:text-[#000000] ">
+            {dropdownValues[typeRequested].map((val, i) => {
+              return (
+                <div
+                  id={i}
+                  className="border-b flex items-center align-middle justify-center "
+                >
+                  <div
+                    id={i}
+                    className="w-full m-0.5 cursor-pointer rounded-md hover:bg-[#f4c301] dark:hover:bg-gray-200"
+                  >
+                    <span>{val}</span>
+                  </div>
+                </div>
+              );
+            })}
           </div>
-        );
-      })}
+        ) : null}
+      </div>
     </div>
   );
 };
